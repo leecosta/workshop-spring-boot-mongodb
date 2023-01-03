796edf01cc8922da0791f61464c3bb11d6dc7049
Letícia Costa Mancini <leticia.mancini@ibm.com>
2023-01-03 17:01:09 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/lecosta/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.lecosta.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.lecosta.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+
+}
