15f27812ff58947533e5c477318655e41dc15032
Letícia Costa Mancini <leticia.mancini@ibm.com>
2023-01-04 11:34:46 -0300

    Consulta simples com query methods

--- src/main/java/com/lecosta/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/lecosta/workshopmongo/repository/PostRepository.java
@@ -1,11 +1,14 @@
 package com.lecosta.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.lecosta.workshopmongo.domain.Post;
 
 @Repository
-public interface PostRepository extends MongoRepository<Post, String>{
+public interface PostRepository extends MongoRepository<Post, String> {
 
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
